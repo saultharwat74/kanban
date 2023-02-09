7335d3f0c30222b42910fd51c00fb02155ff9745
saul74 <mohamedtharout190@gmail.com>
2023-02-09 06:52:40 +0200

    feat(plugins): adding vuetify

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,19 @@
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Vuetify
+import { createVuetify } from "vuetify";
+
+const myCustomLightTheme = {
+  dark: false,
+  
+};
+export default createVuetify({
+  theme: {
+    defaultTheme: "myCustomLightTheme",
+    themes: {
+      myCustomLightTheme,
+    },
+  },
+});
