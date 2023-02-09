99adb233a32e7d05a3b2a6246a293b7684cd55b5
saul74 <mohamedtharout190@gmail.com>
2023-02-09 07:59:49 +0200

    fix(src->components->tasks): fixing ui and adding ghost-class to draggable component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -33,11 +33,12 @@ const change = {
       class="rounded-tl-3xl rounded-tr-3xl rounded-br-3xl px-2 text-white text-xl font-medium"
       >{{ statusText[props.status] }}
     </span>
-    <div class="flex-col mx-2 bg-white w-full h-[70vh] overflow-y-auto">
+    <div class="flex-col my-2 px-3 rounded-md bg-gradient-to-r from-[#b0d0e2] to-[#88AFC3] w-full h-[70vh] overflow-y-auto">
       <draggableComponent
         :list="store.tasks[props.status]"
         @change="change[props.status]"
         group="any"
+        ghost-class="opacity-0"
         itemKey="id"
       >
         <template  #item="{ element, index }">
